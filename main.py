093244840007df1df4ca10cdde813096c79a977f
Vann4 <Vann4>
2023-09-07 21:37:06 +0300

    Создание нового url пути, seasons

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from enum import Enum
 
-class Seasons(str, Enum):
+class Seasons(str, Enum): #класс сезонов природы
     spring = 'spring' #весна
     summer = 'summer' #лето
     fall = 'fall' #осень
@@ -12,3 +12,8 @@ app = FastAPI()
 @app.get('/')
 async def root():
     return {'Hello'}
+
+@app.get('/seasons/{season_name}')
+async def seasons(season_name: Seasons):
+    if season_name is Seasons.spring: #Если season_name равен Seasons.spring ('spring'), то сработает return
+        return {"Название сезона": 'Весна', 'message': 'Поздравляю, вы попали на сезон весны'}
