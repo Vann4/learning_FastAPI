5ebcc39b1c6527b1e4ab9c7edfd03d7632722817
Vann4 <Vann4>
2023-09-07 21:50:58 +0300

    Добавление перечислений по url seasons, с использованием сравнения и получения значений перечисления

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ async def root():
     return {'Hello'}
 
 @app.get('/seasons/{season_name}')
-async def seasons(season_name: Seasons):
+async def seasons(season_name: Seasons): #Сравнение элементов перечисления
     if season_name is Seasons.spring: #Если season_name равен Seasons.spring ('spring'), то сработает return
-        return {"Название сезона": 'Весна', 'message': 'Поздравляю, вы попали на сезон весны'}
+        return {'Название сезона': 'Весна', 'Сообщение': 'Поздравляю, вы попали на сезон весны'} #Возврат элементов перечисления
+    if season_name.value == 'summer': #Получение значения перечисления
+        return {'Название сезона': 'Лето', 'Сообщение': 'Поздравляю, вы попали на сезон лета'} #Возврат элементов перечисления
+    if season_name is Seasons.fall:
+        return {'Название сезона': 'Осень', 'Сообщение': 'Поздравляю, вы попали на сезон осени'}
+    if season_name.value == 'winter':
+        return {'Название сезона': 'Зима', 'Сообщение': 'Поздравляю, вы попали на сезон зимы'}
