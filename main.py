82c80d4d7e028bd8115cdef95abd0c66a3c0f989
Vann4 <Vann4>
2023-09-07 21:20:48 +0300

    Создание класса сезонов природы

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
+from enum import Enum
+
+class Seasons(str, Enum):
+    spring = 'spring' #весна
+    summer = 'summer' #лето
+    fall = 'fall' #осень
+    winter = 'winter' #зима
 
 app = FastAPI()
 
